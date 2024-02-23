c52abb3de99b1c32643459397658558098a104fd
danilo <danilo.nikolic@clockify.me>
2024-02-24 00:14:57 +0100

    dsds

---
+++ WPFPoslasticarnica/Konekcija.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPFPoslasticarnica
+{
+    class Konekcija
+    {
+        public SqlConnection KreirajKonekciju()
+        {
+            SqlConnectionStringBuilder ccnSb = new SqlConnectionStringBuilder();
+            ccnSb.DataSource = @"Vuk\SQLEXPRESS";
+            ccnSb.InitialCatalog = "Poslasticarnica";
+            ccnSb.IntegratedSecurity = true;
+            string con = ccnSb.ToString();
+            SqlConnection konekcija = new SqlConnection(con);
+            return konekcija;
+        }
+    }
+}
